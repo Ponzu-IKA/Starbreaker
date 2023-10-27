0b6525184a6f0d62e38ab033e3a3a88d955931d0
FalFal <>
2023-10-27 22:47:40 +0900

    Initialize Commit

---
+++ src/main/java/ponzu_ika/starbreaker/StarBreaker.java
@@ -0,0 +1,10 @@
+package ponzu_ika.starbreaker;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = StarBreaker.MODID,name = StarBreaker.MODNAME,version = StarBreaker.VER)
+public class StarBreaker {
+    public static final String MODID = "starbreaker";
+    public static final String MODNAME = "StarBreaker";
+    public static final String VER="1.0";
+}
